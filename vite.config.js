1b2e32f34cbf84fdfee5818f0a2a990cb68baf82
Azhar Malik <azharmalik@MacBook-Pro.local>
2023-12-24 02:12:47 +0530

    vercell added

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			'/api': {
-				target: 'http://54.66.46.192/',
+				target: 'http://youreply.tech/',
 				changeOrigin: true,
 			},
 		},
